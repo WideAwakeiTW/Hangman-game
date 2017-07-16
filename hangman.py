f987d9e41f2cf79464e2bec0d974049bcd9ef724
Beth Hart <errordrivendev@gmail.com>
2017-07-16 15:36:18 -0700

    add play again logic and refactor code

--- hangman.py
+++ hangman.py
@@ -2,54 +2,75 @@ import random
 import string
 
 #open text file of words, save as a list and randomly select a word for play
-with open("words.txt", "r") as word_obj:
-    contents = word_obj.read()
-    words = contents.split()
-    play_word = random.choice(words)
+def new_word():
+        with open("words.txt", "r") as word_obj:
+            contents = word_obj.read()
+            words = contents.split()
+            word = random.choice(words)
+            return word
 
-print(play_word)
+def letters_display(secret_word):        
+        result = ["_"] * len(secret_word)
+        print("Your new word has " + str(len(secret_word)) +" letters.")
+        return result
 
-letters_list = list(play_word)
-result = ["_"] * len(letters_list)
+def remaining_letters(used, alpha):
+        if used in alpha:
+                alpha.remove(used)
+                return alpha
+        else:
+             print("\nOK DING-DONG! You have already used that letter -- try again!")
 
-print("Your new word has " + str(len(letters_list)) +" letters.")
-alpha = list(string.ascii_lowercase)
-def remaining_letters(used):
-        alpha.remove(used)
-        return alpha
- 
-count = 6
-while letters_list != result:
-    print("\nHere are the letters you have to choose from:")
-    for letter in alpha:
-        print (letter, end="")
-    guess = input("\n\nGuess a letter: ").lower()
-    if guess in alpha:
-        remaining_letters(guess)
-    else:
-        print("\nOK DING-DONG! You have already used that letter -- try again!")
-        continue
+def hangman():
+        alpha = list(string.ascii_lowercase)
+        count = 6
+        play_word = new_word()
+        letters_list = list(play_word)
+        result = letters_display(play_word)
+        print(play_word)
+        while letters_list != result:
+            print("\nHere are the letters you have to choose from:")
+            for letter in alpha:
+                        print (letter, end="" + " ")
+
+            guess = input("\n\nGuess a letter: ").lower()
+                                           
+            if guess not in string.ascii_lowercase or guess == "":
+                print("Uh...Letter please!")
+            elif guess in letters_list and guess in alpha:
+                print("\nWOOT-WOOT!! You guessed a letter!\n")
+                for index, char in enumerate(letters_list):
+                    if char == guess:
+                        result[index] = guess
+                for char in result:
+                    print (char, end=" ",)
+                print("\n")
+                remaining_letters(guess, alpha)
+            elif count > 0 and guess in alpha:
+                print("\nThat letter is not in the word. Try Again!")
+                count -= 1
+                remaining_letters(guess, alpha)
+            elif count == 0:
+                print ("\nHANGMAN!")
+                print("The word was: " + play_word)
+                play_again()
+                break
+            else:
+                remaining_letters(guess, alpha)
+        if letters_list == result:
+            print("NICE JOB!! YOU WON!")
+            play_again()
+          
+def play_again():
+        play_again = input("Do you want to play again? Type 'y' for yes, 'n' for no: ")
+        if play_again == 'y':
+            hangman()
+        else:
+           quit
+hangman()
+            
         
-    if guess in letters_list:
-        print("\nCorrect! You guessed a letter!\n")
-
-        for index, char in enumerate(letters_list):
-            if char == guess:
-                result[index] = guess
-        for char in result:
-            print (char, end=" ",)
-        print("\n")
-
-    elif count > 0:
-        print("\nThat letter is not in the word. Try Again!")
-        count -= 1
-    elif count == 0:
-        print ("\nHANGMAN!")
-        print("The word was: " + play_word)
-        break
-if letters_list == result:
-    print("YOU WON!")   
-
+         
   
 
 
